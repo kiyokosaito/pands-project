9d5d2dc6a8cd24b4114e5e62ade39c4fcdef3609
Kiyoko Saito <kiyoko_saito@Kiyokos-MacBook-Pro.local>
2019-04-28 12:59:17 +0900

    added comment in sepal_width_ave.py

--- sepal_width_ave.py
+++ sepal_width_ave.py
@@ -7,7 +7,9 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('iris.data.csv',header=None)
 
+#this is to print average of sepal length.
 print((np.mean(df.iloc[:150,1])),'average of sepal_width') 
 
+# these to pring min and max of sepal_length.
 print(min(df.iloc[:150,1]),'minimum of sepal_width')
 print(max(df.iloc[:150,1]),'maximum of sepal_width')
