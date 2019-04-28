f9dffd65f562745c1edc250d0729cbca091acffe
Kiyoko Saito <kiyoko_saito@Kiyokos-MacBook-Pro.local>
2019-04-28 12:48:18 +0900

    modified petal_width_ave.py

--- petal_width_ave.py
+++ petal_width_ave.py
@@ -7,4 +7,11 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('iris.data.csv',header=None)
 
-print((np.mean(df.iloc[:149,3])),'average of petal_width') 
+# This is to calculate the avarage of petal_width.
+print((np.mean(df.iloc[:150,3])),'average of petal_width') 
+
+# these to pring min and max of petal_width.
+print(min(df.iloc[:150,3]),'minimum of petal_width')
+print(max(df.iloc[:150,3]),'maximum of petal_width')
+
+
